70ccc2224280a4c0f257b2c47f4215a0d9576006
Jun Takeda <takeda.jun@gmail.com>
2019-12-04 01:31:47 +0900

    fix: code format

--- micropython/rn42.py
+++ micropython/rn42.py
@@ -3,15 +3,13 @@ class RN42:
         # Set up RN42
         self.uart = uart
 
-
     def write(self, key):
         self.uart.write(key)
-
 
     def send_keyboard_report(self, data, modifier):
         if len(data) != 6:
             return
-        
+
         header = bytearray(b'\xFD\x09\x01\x00\x00')
         header[3] = modifier
         self.uart.write(header)
